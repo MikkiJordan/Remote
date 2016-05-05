08e3c83def6c88a241e93776178cc0f6326771e3
MikkiJordan <rasmus.j.v@hotmail.com>
2016-05-05 12:34:41 +0200

    ReV 0.2
    Including code for the infered LED, so that it will only receive 1 volt
    and something like .25-.5 ampere

--- PointerRemote.ino
+++ PointerRemote.ino
@@ -1,29 +1,29 @@
 void setup() {
-  Serial.begin(9600);
   // put your setup code here, to run once:
+  Serial.begin(9600);
   pinMode(4,OUTPUT); // Red LED output
   pinMode(5,OUTPUT); // Green LED output
   pinMode(2,OUTPUT); // Button output 
+  pinMode(6, OUTPUT); // Infered LED output
 }
 
 void loop() {
-  int ButtonInteraction = analogRead(0);
+  int ButtonInteraction = digitalRead(3);
   // put your main code here, to run repeatedly:
 
-  if(ButtonInteraction == 0){
+  if(ButtonInteraction == LOW){
+    analogWrite(6,0);
     analogWrite(5,0);
-    analogWrite(4,255);   
-    delay(500);      
-    Serial.println("The device is ready for use");
- //   Serial.println(ButtonInteraction);
+    analogWrite(4,200);   
+    delay(100);      
+    Serial.println("The device is ready for use...");
   }
 
-  else if (ButtonInteraction > 120){
-    analogWrite(5,255);
-    analogWrite(4,0);        
-    delay(500);
-    Serial.println("The device is being used");
-//    Serial.println(ButtonInteraction);
-
+  else if (ButtonInteraction == HIGH){
+    analogWrite(6,50);
+    analogWrite(5,200);
+    analogWrite(4,0);       
+    delay(100);
+    Serial.println("The device is being used...");
   }
 }
